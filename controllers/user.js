6d2d2c49e88450a9331185ef662739a712105dbe
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-07-29 15:16:43 +0300

    Update User data

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,48 @@
-export const test = (req, res) => {
+import { createError } from "../error.js"
+import User from '../models/User.js'
+
+export const update = async (req, res, next) => {
+    if (req.params.id === req.user.id) {
+        try {
+            const updatedUser = await User.findByIdAndUpdate(req.params.id, 
+                {
+                    $set:req.body
+                },
+                { new: true}
+            )
+            res.status(200).json({
+                success: true,
+                status: 200,
+                user : updatedUser
+            })
+        } catch (error) {
+            next(error)
+        }
+    } else {
+        return next(createError(403, 'You can update only your account'))
+    }
+}
+
+export const delete_user = (req, res, next) => {
+    res.json('Its Succesfull')
+}
+
+export const getUser = (req, res, next) => {
+    res.json('Its Succesfull')
+}
+
+export const subscribe = (req, res, next) => {
+    res.json('Its Succesfull')
+}
+
+export const unsubscribe = (req, res, next) => {
+    res.json('Its Succesfull')
+}
+
+export const like = (req, res, next) => {
+    res.json('Its Succesfull')
+}
+
+export const dislike = (req, res, next) => {
     res.json('Its Succesfull')
 }
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,21 @@
 import express from 'express'
-import { test } from '../controllers/user.js'
+import { delete_user, dislike, getUser, like, subscribe, unsubscribe, update } from '../controllers/user.js'
+import verifyToken from '../verifyToken.js'
 
 const router = express.Router()
 
-router.get('/test', test)
+router.put('/:id', verifyToken, update)
+
+router.delete('/:id', delete_user)
+
+router.get('/find/:id', getUser)
+
+router.put('/sub/:id', subscribe)
+
+router.put('/unsub/:id', unsubscribe)
+
+router.put('/like/:videoId', like)
+
+router.put('/dislike/:videoId', dislike)
 
 export default router
